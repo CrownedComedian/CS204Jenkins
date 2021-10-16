6d8872609d40d85db7cef54bf5f8dcce33ca84c6
CrownedComedian <crownedcomedian@gmail.com>
2021-10-16 13:48:37 -0600

    added breaks to switch statement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,21 @@ public class Main {
         switch (args[0]) {
             case "add":
                 System.out.println(c.add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                break;
             case "subtract":
                 System.out.println(c.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                break;
             case "multiply":
                 System.out.println(c.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                break;
             case "divide":
                 System.out.println(c.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                break;
             case "binary":
                 System.out.println(c.intToBinaryNumber(Integer.parseInt(args[1])));
+                break;
+            default:
+                System.out.println("command not recognized");
         }
     }
 }
